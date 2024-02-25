5841abff3e7ab5d3e2a3e7c4985bcca197ee45f5
Julio M Cruz <julio.cruz@eb-ms.net>
2024-02-25 14:16:55 -0700

    Update UI

---
+++ NutriKnowWeb/pages/index.tsx
@@ -0,0 +1,16 @@
+import { ConnectButton } from '@rainbow-me/rainbowkit';
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
+
+const Home: NextPage = () => {
+  return (
+    <div className={styles.container}>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+    </div>
+  );
+};
+
+export default Home;
