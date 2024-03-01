9bcc11bf840ec55ea914910b1ec1f6d128436de3
raigal-r <raquel.raigal@outlook.es>
2024-03-01 13:26:53 -0700

    todo

--- llama/app/llamaChatPage.tsx
+++ llama/app/llamaChatPage.tsx
@@ -1,7 +1,7 @@
 import Header from "@/app/components/header";
 import ChatSection from "./components/chat-section";
 
-export default function Home() {
+export default function LLamaChatPage() {
   return (
     <main className="flex min-h-screen flex-col items-center gap-10 p-24 background-gradient">
       <Header />
