c967555fb7b0f6466627f5bdbbab4f07ad3c78b2
Julio M Cruz <julio.cruz@eb-ms.net>
2024-03-01 20:53:21 -0700

    Update NutriKnowMembers.sol

--- scaffold-eth-2-subgraph-package/packages/hardhat/contracts/NutriKnowMembers.sol
+++ scaffold-eth-2-subgraph-package/packages/hardhat/contracts/NutriKnowMembers.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-// contract address 0x4C3139A3bDf6AeC62d8b65B053e41cd738b41e8F in Base Sepolia
+// contract address 0xd8b934580fcE35a11B58C6D73aDeE468a2833fa8 in Base Sepolia
 
 contract MemberManagement {
     struct Member {
@@ -12,9 +12,9 @@ contract MemberManagement {
 
     enum Role { Admin, Nutritionist, User }
 
-    // Events
     event MemberAdded(address indexed memberAddress, uint height, uint weight, uint bornYear, Role role);
     event MemberRoleChanged(address indexed memberAddress, Role newRole);
+    event MemberUpdated(address indexed memberAddress, uint height, uint weight, uint bornYear);
 
     mapping(address => Member) public members;
     mapping(address => Role) public roles;
@@ -25,24 +25,42 @@ contract MemberManagement {
         _;
     }
 
+    modifier adminOrMember(address _memberAddress) {
+        require(
+            msg.sender == _memberAddress || roles[msg.sender] == Role.Admin,
+            "Only the member or an admin can perform this action"
+        );
+        _;
+    }
+
     constructor() {
         owner = msg.sender;
-        roles[msg.sender] = Role.Admin; // Setting the contract deployer as an Admin
-        // Emitting an event for the contract deployer being added as an Admin
+        roles[msg.sender] = Role.Admin;
         emit MemberAdded(msg.sender, 0, 0, 0, Role.Admin);
     }
 
     function addMember(address _memberAddress, uint _height, uint _weight, uint _bornYear, Role _role) public onlyAdmin {
         members[_memberAddress] = Member(_height, _weight, _bornYear);
         roles[_memberAddress] = _role;
-        // Emit an event when a new member is added
         emit MemberAdded(_memberAddress, _height, _weight, _bornYear, _role);
     }
 
     function setMemberRole(address _memberAddress, Role _newRole) public onlyAdmin {
         require(_memberAddress != owner, "Cannot change the role of the contract owner");
         roles[_memberAddress] = _newRole;
-        // Emit an event when a member's role is changed
         emit MemberRoleChanged(_memberAddress, _newRole);
     }
+
+    function updateMember(address _memberAddress, uint _height, uint _weight, uint _bornYear) public adminOrMember(_memberAddress) {
+        members[_memberAddress].height = _height;
+        members[_memberAddress].weight = _weight;
+        members[_memberAddress].bornYear = _bornYear;
+        emit MemberUpdated(_memberAddress, _height, _weight, _bornYear);
+    }
+
+    function getMemberDetails(address _memberAddress) public view returns (uint, uint, uint) {
+        Member memory member = members[_memberAddress];
+        return (member.height, member.weight, member.bornYear);
+    }
+    
 }
